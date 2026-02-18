1e74a04ff8492f775ca38456cfdc7fd1d3d7a574
Boss <penielkouboumor@gmail.com>
2026-02-18 08:37:46 +0000

    Mise en place des villes

--- geo/formulaire.py
+++ geo/formulaire.py
@@ -1,10 +1,26 @@
 from django import forms
 from .models import patrimoine
 
+Ville = [
+    ("Lome", "Lomé"),
+    ("Kara", "Kara"),
+    ("Sokode", "Sokodé"),
+    ("Atakpame", "Atakpamé"),
+    ("Kpalime", "Kpalimé"),
+    ("Aneho", "Aného"),
+    ("Tsevie", "Tsévié"),
+    ("Dapaong", "Dapaong"),
+    ("Bassar", "Bassar"),
+    ("Notse", "Notsé"),
+    ("Badou", "Badou"),
+]
+
 class patrimoineForm(forms.ModelForm):
+    ville = forms.ChoiceField(choices=Ville,required=False,label="ville")
+
     class Meta:
         model = patrimoine
-        fields = ['nom','latitude', 'longitude']
+        fields = ['Ville','nom','latitude', 'longitude']
         widgets = {
             'nom': forms.TextInput(attrs={'class': 'form-control'}),
             'latitude': forms.NumberInput(attrs={'class': 'form-control'}),
