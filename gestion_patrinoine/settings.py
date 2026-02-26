1dec486f939802dfff284618a0d7e7e4df634009
edem-wilfried-amouzou <edemwilfriedamouzou@gmailcom>
2026-02-26 11:57:03 +0000

    Allowed host change

--- gestion_patrinoine/settings.py
+++ gestion_patrinoine/settings.py
@@ -48,7 +48,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['gestion-patrimoine-b3hj.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
